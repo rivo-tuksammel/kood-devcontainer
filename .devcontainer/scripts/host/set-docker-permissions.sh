b67def3ccde14e5747549556e4988b3b0ceb2e80
rivo.tuksammel <rivo.tuksammel@gmail.com>
2025-04-26 12:41:35 +0000

    Set docker permissions conditionally

--- .devcontainer/scripts/host/set-docker-permissions.sh
+++ .devcontainer/scripts/host/set-docker-permissions.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 echo "Making sure docker permissions are set up correctly..."
-echo "Please enter your password if prompted to allow running this script with sudo."
-sudo getent group docker || sudo groupadd docker
-sudo usermod -aG docker $USER
-sudo chown root:docker /var/run/docker.sock
+
+if [ "$(stat -c '%G' /var/run/docker.sock)" != "docker" ]; then
+    echo "Setting permissions for Docker socket..."
+    echo "Please enter your password if prompted to allow running this script with sudo."
+    sudo getent group docker || sudo groupadd docker
+    sudo usermod -aG docker $USER
+    sudo chown root:docker /var/run/docker.sock
+else 
+    echo "Docker socket permissions are already set up correctly. skipping..."
+fi
