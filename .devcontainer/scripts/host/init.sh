799d5aa583439606fa7e8d1a6e42e081986e3ae1
rivo.tuksammel <rivo.tuksammel@gmail.com>
2025-04-27 09:08:45 +0300

    Remove unused .env file and update scripts to improve Docker permissions setup

--- .devcontainer/scripts/host/init.sh
+++ .devcontainer/scripts/host/init.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+
+cat << "EOF"
+  _  __               _     _______              _____            _        _                 
+ | |/ /              | |   / /  __ \            / ____|          | |      (_)                
+ | ' / ___   ___   __| |  / /| |  | | _____   _| |     ___  _ __ | |_ __ _ _ _ __   ___ _ __ 
+ |  < / _ \ / _ \ / _` | / / | |  | |/ _ \ \ / / |    / _ \| '_ \| __/ _` | | '_ \ / _ \ '__|
+ | . \ (_) | (_) | (_| |/ /  | |__| |  __/\ V /| |___| (_) | | | | || (_| | | | | |  __/ |   
+ |_|\_\___/ \___/ \__,_/_/   |_____/ \___| \_/  \_____\___/|_| |_|\__\__,_|_|_| |_|\___|_|   
+
+EOF
+
 # This script is used to extract the host user information (UID, GID, and username)
 bash .devcontainer/scripts/host/extract-env.sh
 bash .devcontainer/scripts/host/set-docker-permissions.sh
--- .devcontainer/scripts/host/set-docker-permissions.sh
+++ .devcontainer/scripts/host/set-docker-permissions.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-echo "Making sure docker permissions are set up correctly..."
+# This script is used to set up Docker permissions for the host user.
+# It checks if the Docker socket is owned by the 'docker' group and if not, it adds the current user to the 'docker' group.
+# It also sets the ownership of the Docker socket to 'root:docker'.
+# This script should be run with sudo privileges.
 
+echo "Making sure docker permissions are set up correctly..."
 if [ "$(stat -c '%G' /var/run/docker.sock)" != "docker" ]; then
     echo "Setting permissions for Docker socket..."
     echo "Please enter your password if prompted to allow running this script with sudo."
